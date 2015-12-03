02dba94ada4726f4916a651482ec228c3b461a50
Phuong Doan <pdoan@atlassian.com>
2015-12-03 20:48:22 +0700

    milestone 5a

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151203120657) do
+ActiveRecord::Schema.define(version: 20151203133616) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "author"
+    t.string   "comment"
+    t.integer  "photo_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "comments", ["photo_id"], name: "index_comments_on_photo_id", using: :btree
 
   create_table "photos", force: :cascade do |t|
     t.string   "username"
@@ -22,4 +32,5 @@ ActiveRecord::Schema.define(version: 20151203120657) do
     t.datetime "updated_at",  null: false
   end
 
+  add_foreign_key "comments", "photos"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,9 @@
 (1..100).each do |i|
 	Photo.create(username: Faker::Name.name, caption: Faker::Lorem.paragraph, likes_count: Faker::Number.number(4), url: "https://unsplash.it/400/300?image=#{i}")
 end
+
+Photo.all.each do |photo|
+  rand(10).times do
+    photo.comments << Comment.new(author: Faker::Name.name, comment: Faker::Lorem.paragraph)
+  end
+end
